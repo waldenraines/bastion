f7e8b173595d0f14a36463ec06e5192107a55b97
Walden Raines <walden@redhat.com>
2016-07-28 15:50:03 -0400

    Refs #15665: add tests for ui-router config.
    This commit adds tests for ui-router config which necessitated the
    reorganization of several bastion modules and their dependencies.
    
    http://projects.theforeman.org/issues/15665

---
+++ app/assets/javascripts/bastion/routing.module.js
@@ -0,0 +1,68 @@
+angular.module('Bastion.routing', ['ui.router']);
+
+(function () {
+    'use strict';
+
+    /**
+     * @ngdoc config
+     * @name  Bastion.routing.config
+     *
+     * @requires $urlRouterProvider
+     * @requires $locationProvider
+     *
+     * @description
+     *   Routing configuration for Bastion.
+     */
+    function bastionRouting($urlRouterProvider, $locationProvider) {
+        var oldBrowserBastionPath = '/bastion#';
+
+        $urlRouterProvider.rule(function ($injector, $location) {
+            var $sniffer = $injector.get('$sniffer'),
+                $window = $injector.get('$window'),
+                path = $location.path();
+
+            if (!$sniffer.history) {
+                $window.location.href = oldBrowserBastionPath + $location.path();
+            }
+
+            // removing trailing slash to prevent endless redirect
+            if (path[path.length - 1] === '/') {
+                return path.slice(0, -1);
+            }
+        });
+
+        $urlRouterProvider.otherwise(function ($injector, $location) {
+            var $window = $injector.get('$window'),
+                $state = $injector.get('$state'),
+                rootPath = $location.path().split('/')[1],
+                url = $location.absUrl(),
+                foundParentState;
+
+            // ensure we don't double encode +s
+            url = url.replace(/%2B/g, "+");
+
+            // Remove the old browser path if present
+            url = url.replace(oldBrowserBastionPath, '');
+
+            if (rootPath) {
+                rootPath = rootPath.replace('_', '-');
+                foundParentState = _.find($state.get(), function (state) {
+                    return state.url && state.url.replace('/', '') === rootPath;
+                });
+            }
+
+            if (foundParentState) {
+                $window.location.href = '/404';
+            } else {
+                $window.location.href = url;
+            }
+            return $location.path();
+        });
+
+        $locationProvider.html5Mode(true);
+
+    }
+
+    angular.module('Bastion.routing').config(bastionRouting);
+    bastionRouting.$inject = ['$urlRouterProvider', '$locationProvider'];
+})();
